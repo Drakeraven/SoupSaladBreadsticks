11ec5d101401d8f70d722731e8b5966ef52014d0
steph <steph@DESKTOP-CK9F15N>
2017-10-13 12:10:13 -0700

    Skeleton Done.

--- EasterEggProject/src/EasterEgg.java
+++ EasterEggProject/src/EasterEgg.java
@@ -1,9 +1,41 @@
-
+/**
+ * The Easter Egg  Project.
+ * @author Soup, Salad, and Breadsticks
+ * @version 1.0
+ */
 public class EasterEgg {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		startingEgg();
+		//TODO: Everyone call their methods
+		//TODO: Everyone write java doc for the method.
 
 	}
 
+	public static void startingEgg() {
+		System.out.println("We Are Soup, Salad, and Breadsticks.");
+		
+	}
+	
+	public static void BryanEgg() {
+		
+	}
+	
+	/**
+	 * This is Stephanie's message.
+	 * Pre condition: None
+	 * Post Condition: Message Displayed
+	 */
+	public static void StephanieEgg() {
+		System.out.println("I'm Stephanie, I'll only eat the breadsticks.");
+		
+	}
+	
+	public static void CassieEgg() {
+		
+	}
+	
+	public static void CynthiaEgg() {
+		
+	}
 }
