3e86a9b23e95da7cfc4a582d5f34231d5c795c24
cynthiam4 <cynthiamora97@gmail.com>
2017-12-03 04:37:03 -0800

    Started BillTracker and Project Actions

---
+++ DIYGuyApp/src/actions/BillTrackerAction.java
@@ -0,0 +1,29 @@
+/**
+ * 
+ */
+package actions;
+
+import java.awt.event.ActionEvent;
+
+import javax.swing.AbstractAction;
+
+/**
+ * @author Cynthia Mora Olmedo
+ *
+ */
+public final class BillTrackerAction extends AbstractAction{
+
+	/**
+	 * 
+	 */
+	public BillTrackerAction() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ DIYGuyApp/src/actions/ProjectsAction.java
@@ -0,0 +1,29 @@
+/**
+ * 
+ */
+package actions;
+
+import java.awt.event.ActionEvent;
+
+import javax.swing.AbstractAction;
+
+/**
+ * @author Cynthia Mora Olmedo
+ *
+ */
+public final class ProjectsAction extends AbstractAction{
+
+	/**
+	 * 
+	 */
+	public ProjectsAction() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
