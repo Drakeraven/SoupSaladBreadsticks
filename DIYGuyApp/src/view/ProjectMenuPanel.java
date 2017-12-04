a5e0c16f626483e65d8a0ba0c0cc7a51bb629b53
HalfTau <bryansands@student.olympic.edu>
2017-12-04 01:13:34 -0800

    wow, bryan actually did something (not really): made 3 classes, wow

---
+++ DIYGuyApp/src/view/ProjectMenuPanel.java
@@ -0,0 +1,24 @@
+package view;
+
+import java.awt.Dimension;
+
+import javax.swing.JPanel;
+
+public class ProjectMenuPanel extends JPanel{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	public static final int WIDTH = 500;
+	public static final int HEIGHT = 500;
+	
+	public ProjectMenuPanel() {
+		setupGUI();
+	}
+
+	private void setupGUI() {
+		// TODO Auto-generated method stub
+		
+	}
+}
