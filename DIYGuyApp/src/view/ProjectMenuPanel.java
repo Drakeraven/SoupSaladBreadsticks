a662f6ce560867f636a4840810d6e99a0c10df83
HalfTau <bryansands@student.olympic.edu>
2017-12-05 13:33:36 -0800

    temp table placeholder

--- DIYGuyApp/src/view/ProjectMenuPanel.java
+++ DIYGuyApp/src/view/ProjectMenuPanel.java
@@ -2,23 +2,52 @@ package view;
 
 import java.awt.Dimension;
 
+import javax.swing.JButton;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTable;
+import javax.swing.table.TableColumn;
+import javax.swing.table.TableModel;
 
 public class ProjectMenuPanel extends JPanel{
 
-	/**
-	 * @author Bryan
-	 */
+	/**Default serial number*/
 	private static final long serialVersionUID = 1L;
-	public static final int WIDTH = 500;
-	public static final int HEIGHT = 500;
+	private static final int WIDTH = 500;
+	private static final int HEIGHT = 500;
+	
+	JLabel titleLbl;
+	JTable projectTbl;
 	
 	public ProjectMenuPanel() {
+		titleLbl = new JLabel("PROJECTS");
+		titleLbl.setSize(WIDTH/2, HEIGHT/20);
+
 		setupGUI();
+
 	}
 
 	private void setupGUI() {
 		// TODO Auto-generated method stub
+		setSize(WIDTH, HEIGHT);
+		this.add(titleLbl);
+		String[] columnNames = {"",
+				"",
+				""};
+		Object[][] data = {
+			    {"Kathy", "Smith",
+			     "Snowboarding", new Integer(5), new Boolean(false)},
+			    {"John", "Doe",
+			     "Rowing", new Integer(3), new Boolean(true)},
+			    {"Sue", "Black",
+			     "Knitting", new Integer(2), new Boolean(false)},
+			    {"Jane", "White",
+			     "Speed reading", new Integer(20), new Boolean(true)},
+			    {"Joe", "Brown",
+			     "Pool", new Integer(10), new Boolean(false)}
+			};
+		projectTbl = new JTable(data,columnNames);
+		this.add(projectTbl);
 		
 	}
 }
