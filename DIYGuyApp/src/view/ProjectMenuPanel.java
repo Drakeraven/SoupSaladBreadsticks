bb596fa63889a31f607b4b460bf43ce5ea6863a0
HalfTau <bryansands@student.olympic.edu>
2017-12-05 18:54:36 -0800

    threw away the table

--- DIYGuyApp/src/view/ProjectMenuPanel.java
+++ DIYGuyApp/src/view/ProjectMenuPanel.java
@@ -1,5 +1,7 @@
 package view;
 
+import java.awt.BorderLayout;
+
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -18,8 +20,7 @@ public class ProjectMenuPanel extends JPanel{
 	
 	private Project[] projects;
 	private JLabel titleLbl;
-	private JTable projectTbl;
-	
+
 	public ProjectMenuPanel() {
 		titleLbl = new JLabel("PROJECTS");
 		titleLbl.setSize(WIDTH/2, HEIGHT/20);
@@ -36,27 +37,11 @@ public class ProjectMenuPanel extends JPanel{
 	private void setupGUI() {
 		// TODO Auto-generated method stub
 		setSize(WIDTH, HEIGHT);
-		
+		this.setLayout(new BorderLayout());
 		
 		this.add(titleLbl);
-		String[] columnNames = {"",
-				"",
-				""};
-		Object[][] data = {
-			    {"Kathy", "Smith",
-			     "Snowboarding", new Integer(5), new Boolean(false)},
-			    {"John", "Doe",
-			     "Rowing", new Integer(3), new Boolean(true)},
-			    {"Sue", "Black",
-			     "Knitting", new Integer(2), new Boolean(false)},
-			    {"Jane", "White",
-			     "Speed reading", new Integer(20), new Boolean(true)},
-			    {"Joe", "Brown",
-			     "Pool", new Integer(10), new Boolean(false)}
-			};
-		projectTbl = new JTable(data,columnNames);
-		JScrollPane jsp = new JScrollPane(projectTbl);
-		this.add(jsp);
+		JScrollPane jsp = new JScrollPane();
+		this.add(jsp, BorderLayout.NORTH);
 		
 	}
 }
