60aeb14235e9945528b4dd9c9e68cd66ffd342a2
saltybard <cbr.work@hotmail.com>
2017-12-07 12:17:30 -0800

    I think? 希望というかな

--- DIYGuyApp/src/view/BillEntryPanel.java
+++ DIYGuyApp/src/view/BillEntryPanel.java
@@ -1,5 +1,168 @@
+/*
+ * Coder: Cassie Renz
+ */
+
 package view;
 
-public class BillEntryPanel {
+import java.awt.GridLayout;
+
+import javax.swing.Box;
+import javax.swing.JFileChooser;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+import model.Bill;
+import model.FileHandler;
+import model.UserData;
+
+public class BillEntryPanel extends JPanel {
+	
+
+	private static final long serialVersionUID = 1L;
+	public static final int WIDTH = 500;
+	public static final int HEIGHT = 500;
+
+	public BillEntryPanel(FileHandler fileHand) { 	
+		FileHandler handler = fileHand;
+		setUpAddBill(); 	
+	}	
+	
+	/*
+	 * Method to set up the pop up "add bill information" panel 
+	 * Pre: None
+	 * Post: None 
+	 */
+	public JPanel setUpAddBill() { 
+		Bill newBill;
+		double newCost = 0;
+		String newText;
+		int month = 0;
+		int day = 0;
+		int year = 0;
+		JPanel addBill = new JPanel(new GridLayout(7,1));
+		
+		
+		addBill.add(new JLabel("Enter bill type: "));
+		addBill.add(Box.createHorizontalStrut(15));
+		JTextField billType = new JTextField(5); 
+		addBill.add(billType);
+		addBill.add(Box.createHorizontalStrut(15));
+		addBill.add(new JLabel("Enter bill total: "));
+		addBill.add(Box.createHorizontalStrut(15));
+		JTextField billCost = new JTextField(); 
+		addBill.add(billCost);
+		addBill.add(Box.createHorizontalStrut(15));
+		addBill.add(new JLabel("Enter bill date (mm/dd/yyyy): "));
+		addBill.add(Box.createHorizontalStrut(15));
+		JTextField date = new JTextField(); 
+		addBill.add(date);
+		
+		
+		//TODO: Check for valid input
+		//TODO: Check for valid input
+		//TODO: Check for valid input omg cassie don't forget	
+		//TODO: Parse the string, check for valid inputs 
+		 
+		
+		int result = JOptionPane.showConfirmDialog(this, addBill, 
+				"Enter New Bill Information", JOptionPane.OK_CANCEL_OPTION);
+		if (result == JOptionPane.OK_OPTION) {
+			//TODO: Get information here
+			newCost = validateDouble(billCost.getText());
+			
+			String[] dateParts = date.getText().split("/");
+			dateParts = validateArray(dateParts);
+			month = validateInt(dateParts[0]); 
+			day = validateInt(dateParts[1]);
+			year = validateInt(dateParts[2]);
+			
+			newBill = new Bill(billType.getText(), month, day, year, newCost); 
+		} else if (result == JOptionPane.CANCEL_OPTION) {
+			addBill = null; 
+			
+		} else if (result == JOptionPane.CLOSED_OPTION) {
+			addBill = null; 
+		}
+		
+		while (newCost == -1 || month == -1 || day == -1 || year == -1) { 
+			if (newCost == -1){ 
+				newText = "Please enter a valid cost";
+			} else { 
+				newText = "Please enter a valid date";
+			}
+			result = JOptionPane.showConfirmDialog(this, addBill, 
+					newText, JOptionPane.OK_CANCEL_OPTION);
+			if (result == JOptionPane.OK_OPTION) {
+				//TODO: Get information here
+				newCost = validateDouble(billCost.getText());
+				
+				String[] dateParts = date.getText().split("/");
+				dateParts = validateArray(dateParts);
+				month = validateInt(dateParts[0]); 
+				day = validateInt(dateParts[1]);
+				year = validateInt(dateParts[2]);
+				
+				newBill = new Bill(billType.getText(), month, day, year, newCost); 
+				//UserData.setBillList(newBill); use filehandler to grab data
+			}
+		}
+		
+		//newBill = new Bill(billType.getText(), day, month, year, newCost);
+		
+		return addBill; 
+	}
+	
+	/*
+	 * Method for validating int inputs
+	 * Pre: Takes in a string s 
+	 * Post: Returns a valid integer, -1 if invalid input
+	 */
+	private int validateInt(String s) { 
+		int valInt; 
+		
+		try{
+		    valInt = Integer.parseInt(s);
+		}catch (NumberFormatException ex) {
+		    valInt = -1; 
+		}
+		
+		if (valInt < 0)  { 
+			valInt = -1; 
+		}
+	
+		return valInt;
+	}
+	
+	/*
+	 * Method for validating double inputs
+	 * Pre: Takes in a string s 
+	 * Post: Returns a valid double, -1 if invalid input
+	 */
+	private double validateDouble(String s) { 
+		double valDouble; 
+		
+		try{
+		    valDouble = Integer.parseInt(s);
+		}catch (NumberFormatException ex) {
+		    valDouble = -1; 
+		}
+		
+		if (valDouble < 0)  { 
+			valDouble = -1; 
+		}
+	
+		return valDouble;
+	}
+	
+	private String[] validateArray(String[] parts) {
+		int size = parts.length;
+		if(size != 3){ 
+			parts = new String[3];
+		}
+		
+		return parts;
+	}
 
 }
--- DIYGuyApp/src/view/BillTrackerMenuPanel.java
+++ DIYGuyApp/src/view/BillTrackerMenuPanel.java
@@ -1,12 +1,101 @@
+
+/*
+ * Creator: Cassie Renz 
+ * Coder: Cassie Renz
+ */
+
 package view;
 
+import java.awt.BorderLayout;
+
+import java.awt.Component;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.GridLayout;
+import java.util.ArrayList;
+
+import javax.swing.Box;
+import javax.swing.JButton;
+import javax.swing.JCheckBox;
+import javax.swing.JFileChooser;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.JTable;
+import javax.swing.JTextField;
 
-public class BillTrackerMenuPanel extends JPanel{
+import model.Bill;
+import model.FileHandler;
+import model.UserData;
 
+/*
+ * Main class for Bill GUI
+ */
+public class BillTrackerMenuPanel extends JPanel {
+	
 	/**
 	 * 
 	 */
-	private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = -8232879434427235700L;
+	public static final int WIDTH = 500;
+	public static final int HEIGHT = 500;
 
+	public BillTrackerMenuPanel(FileHandler fileCh) { 	
+		FileHandler handler = fileCh;
+		buildGUI(); 	
+	}
+	
+	/*
+	 * Initial set up for the main Bill GUI page
+	 * Pre: None 
+	 * Post: None
+	 */
+	public void buildGUI() { 
+		//ArrayList<Bill> billList = UserData.getBillList();
+ 		this.setSize(WIDTH, HEIGHT);
+		this.setLayout(new BorderLayout());
+		//GridBagConstraints constraint = new GridBagConstraints();
+		
+		//JPanel billInformation = new JPanel(new GridLayout(2,2));
+		//JTable overallTable = new JTable();
+		JButton overallTable = new JButton("Table Placeholder");
+		this.add(overallTable, BorderLayout.CENTER);
+		this.add(setUpAddBillButton(), BorderLayout.SOUTH);
+		
+		/*JPanel checkBoxes = new JPanel(new GridLayout(2,1));
+		JCheckBox elec = new JCheckBox("Electricity");
+		JCheckBox water = new JCheckBox("Water");
+		checkBoxes.add(elec);
+		checkBoxes.add(water);
+		this.add(checkBoxes, BorderLayout.EAST);
+		*/
+		
+		//TODO: Implement table lmao
+		
+		/*
+		 * Header needed above the table
+		 * Possible model needed to define the table 
+		 * Haven't actually implemented table
+		 */
+		
+		
+		
+	}
+	
+	/*
+	 * Method to set up the GUI elements for adding a bill to the current User
+	 * Pre: None
+	 * Post: None
+	 */
+	public JPanel setUpAddBillButton() {
+		JPanel billComponent = new JPanel(); 
+		billComponent.setAlignmentY(BOTTOM_ALIGNMENT);
+		
+		JButton addBill = new JButton("Add Bill");
+		billComponent.add(addBill);
+		
+		return billComponent; 
+		
+	}
+	
 }
