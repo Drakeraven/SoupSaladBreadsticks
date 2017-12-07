0d7ad84747f078917f655eb4fd93ce2c6ef1939a
HalfTau <bryansands@student.olympic.edu>
2017-12-07 10:38:49 -0800

    can I push now, please?

--- DIYGuyApp/src/view/ProjectMenuPanel.java
+++ DIYGuyApp/src/view/ProjectMenuPanel.java
@@ -10,7 +10,6 @@ import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
-import javax.swing.JTable;
 import javax.swing.border.Border;
 import javax.swing.border.EmptyBorder;
 
